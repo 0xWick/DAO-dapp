30438c352b2c66151db0d35660c977ee26205bbf
Wick.py <wickprof@gmail.com>
2022-03-29 16:05:07 +0500

    final-push

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0x22419f94D0d4FABe138De67838082DcF7c3d671a";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
